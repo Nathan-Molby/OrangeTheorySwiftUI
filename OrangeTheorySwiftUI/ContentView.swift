342bc50a550d195420f6a08d6d419045182728f2
Nathan Molby <nathan.molby@willowtreeapps.com>
2025-02-04 21:19:01 -0500

    Initial commit

---
+++ OrangeTheorySwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OrangeTheorySwiftUI
+//
+//  Created by Nathan Molby on 2/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OrangeTheorySwiftUI/OrangeTheorySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrangeTheorySwiftUIApp.swift
+//  OrangeTheorySwiftUI
+//
+//  Created by Nathan Molby on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct OrangeTheorySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
